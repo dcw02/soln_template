176dcf6ce1bc1cc091f3580c8754e5d15079693e
David Wang <git@davidwa.ng>
2024-08-01 03:18:47 -0500

    main.cpp template code

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
-#include <iostream>
+#ifdef LOCAL
+#    include "yastdlib.hpp"
+#else
+#    include <bits/stdc++.h>
+#endif
 
-#include "yastdlib.hpp"
+using namespace std;
+
+void from_file(string name, string in = ".in", string out = ".out") {
+    freopen((name + in).c_str(), "r", stdin);
+    freopen((name + out).c_str(), "w", stdout);
+}
 
 int main() {
-    std::cout << "Hello World" << std::endl;
-    hello_world();
+    cin.tie(nullptr)->sync_with_stdio(false);
+    cin.exceptions(cin.failbit);
 }
